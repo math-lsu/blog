738a14e2dbbf0b9f0073bbcc23cf277d411c3380
Genaro Gonzalez <genaro.gonzalez@gmail.com>
2024-06-24 09:19:13 -0500

    formas and user accounts

--- django_project/settings.py
+++ django_project/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 
+LOGIN_REDIRECT_URL = "home" # new
+LOGOUT_REDIRECT_URL = "home"  # new
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -38,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog.apps.BlogConfig',     # new
+    'accounts.apps.AccountsConfig',
 ]
 
 MIDDLEWARE = [
@@ -122,3 +126,5 @@ STATICFILES_DIRS = [BASE_DIR / "static"]    # new
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
